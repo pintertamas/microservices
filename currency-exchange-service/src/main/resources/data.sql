68a7c28147a582016b962c061eb227e424844dd1
Tomi <pintertamas99@gmail.com>
2022-09-21 19:22:38 +0200

    created currency conversion service

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','HUF',404,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','HUF',405,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'USD','EUR',1,'');
