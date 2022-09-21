24ad66d05233e50bce4e79c03209822a301f5c7f
Pinter Tamas <pintertamas99@gmail.com>
2022-09-21 18:32:02 +0200

    microservices folder with currencyexchangeservice project added

---
+++ currency-exchange-service/src/main/java/com/tamaspinter/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,46 @@
+package com.tamaspinter.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String environment;
+
+    public CurrencyExchange() {
+    }
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        super();
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
